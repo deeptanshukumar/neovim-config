217dbdf1ea47a3d5d9b51b779ca73d1a1202a77e
deeptanshukumar <deeptanshu.kumar13@gmail.com>
2025-12-24 12:31:55 +0530

    added some plugins

---
+++ init.lua
@@ -0,0 +1,39 @@
+-- options
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.g.mapleader = " "
+-- lazy.nvim bootstrap
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    {
+    'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
+     dependencies = { 'nvim-lua/plenary.nvim' }
+    }
+}
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
+
+
+
