28cecb43af9c0de46c62ed552b12c6e0adc5bc93
deeptanshukumar <deeptanshu.kumar13@gmail.com>
2025-12-25 11:41:11 +0530

    added more plugins and structured the folders

--- init.lua
+++ init.lua
@@ -1,9 +1,3 @@
--- options
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.g.mapleader = " "
 -- lazy.nvim bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -13,27 +7,10 @@ if not vim.loop.fs_stat(lazypath) then
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable",
-    lazypath,
+    lazypath,{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   })
 end
 vim.opt.rtp:prepend(lazypath)
-
-local plugins = {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    {
-    'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
-     dependencies = { 'nvim-lua/plenary.nvim' }
-    }
-}
-local opts = {}
-
-require("lazy").setup(plugins, opts)
-
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
-
-
+require("vim-options")
+require("lazy").setup("plugins")
 
